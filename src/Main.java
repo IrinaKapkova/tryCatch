cc5ed725c7d457a61f8d0e55033146bf1b27d6f1
Ирина Капкова <kapiral@rambler.ru>
2023-07-24 00:14:31 +0400

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\Ирина Капкова\\IdeaProjects\\test.txt"));
+            String firstString = reader.readLine();
+            System.out.println(firstString);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Ошибка при вводе/выводе данных из файла!");
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
     }
 }
